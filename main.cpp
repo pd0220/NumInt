b4074f831683436c40ba04e586ddeb0a2a22b8a4
pd0220 <david.pesznyak@gmail.com>
2020-10-17 00:07:06 +0200

    Approximated function added

--- main.cpp
+++ main.cpp
@@ -17,13 +17,24 @@ auto f1 = [](double const &E) {
     return E * t1 * t2 * t3 * t4;
 };
 
-// denumarator
+// denominator
 auto f2 = [](double const &E) {
     double t1 = (E + m_ec2);
     double t2 = (E_0 - E + m_nuc2);
     double t3 = std::sqrt(t1 * t1 - m_ec2 * m_ec2);
     double t4 = std::sqrt(t2 * t2 - m_nuc2 * m_nuc2);
     return t1 * t2 * t3 * t4;
+};
+
+// approximation for testing
+// numerator
+auto f3 = [](double const &E) {
+    return E * std::sqrt(E) * (E_0 - E) * (E_0 - E);
+};
+
+// denominator
+auto f4 = [](double const &E) {
+    return std::sqrt(E) * (E_0 - E) * (E_0 - E);
 };
 
 // integration function
@@ -51,8 +62,11 @@ int main(int, char **)
     // parameters
     double const x0 = 0.;
     double const x1 = E_0;
+    int const n = (int)1e8;
 
     // integration results
+    // test for multiple
+    /*
     for (int i = 4; i <= 8; i++)
     {
         int n = (int)std::pow(10, i);
@@ -63,4 +77,9 @@ int main(int, char **)
         std::cout << result1 / result2 << std::endl;
         std::cout << "------" << std::endl;
     }
+    */
+    // result
+    std::cout << integrate(n, x0, x1, f1) / integrate(n, x0, x1, f2) << std::endl;
+    // approximation
+    std::cout << integrate(n, x0, x1, f3) / integrate(n, x0, x1, f4) << std::endl;
 }
